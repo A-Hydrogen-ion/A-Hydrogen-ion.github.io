8f3fed1ddc160bf7f17da9e59d46f2fd9f6bc72a
A-Hydrogen-ion <2693053350@qq.com>
2025-10-21 12:15:12 +0800

    Site updated: 2025-10-21 12:15:12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1761000244575";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1761020085975";const preCache = ["/images/taichi.png","/images/miku.jpg","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
